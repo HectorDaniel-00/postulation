d9cbefe16f6222da499870a2035f92ea91f0a7b9
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-01-05 17:53:33 -0500

    refactor: eliminar DTOs, entidad y repositorio de tecnologías no utilizados

--- app/src/tecnologies/dto/update-tecnology.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTecnologyDto } from './create-tecnology.dto';
-
-export class UpdateTecnologyDto extends PartialType(CreateTecnologyDto) {}
