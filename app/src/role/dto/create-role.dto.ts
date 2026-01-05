d756d9304c7bd97238f18d1ae3a01c8e028b383e
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-01-05 18:32:30 -0500

    feat: agregar documentación Swagger en CreateRoleDto y RoleController

--- app/src/role/dto/create-role.dto.ts
+++ app/src/role/dto/create-role.dto.ts
@@ -1,6 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-
 export class CreateRoleDto {
+  @ApiProperty({ description: 'Nombre del rol', example: 'Administrador' })
   @IsNotEmpty()
   @IsString()
   name: string;
--- app/src/role/role.controller.ts
+++ app/src/role/role.controller.ts
@@ -10,30 +10,43 @@ import {
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { Message } from 'src/common/decorator';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Roles')
 @Controller('role')
 export class RoleController {
   constructor(private readonly service: RoleService) {}
 
   @Message('Rol creado con exito')
+  @ApiOperation({ summary: 'Crear un nuevo rol' })
+  @ApiResponse({ status: 201, description: 'Rol creado exitosamente.' })
+  @ApiResponse({ status: 403, description: 'Acceso denegado.' })
   @Post()
   create(@Body() dto: CreateRoleDto) {
     return this.service.create(dto);
   }
 
   @Message('Rol encontrado exitosamente')
+  @ApiOperation({ summary: 'Obtener todos los roles' })
+  @ApiResponse({ status: 200, description: 'Lista de roles.' })
   @Get()
   findAll() {
     return this.service.findAll();
   }
 
   @Message('Rol encontrado exitosamente')
+  @ApiOperation({ summary: 'Obtener un rol por nombre' })
+  @ApiResponse({ status: 200, description: 'Detalles del rol.' })
+  @ApiResponse({ status: 404, description: 'Rol no encontrado.' })
   @Get('name')
   findOne(@Body() name: string) {
     return this.service.findByName(name);
   }
 
   @Message('Rol eliminado con exito')
+  @ApiOperation({ summary: 'Eliminar un rol por ID' })
+  @ApiResponse({ status: 200, description: 'Rol eliminado exitosamente.' })
+  @ApiResponse({ status: 404, description: 'Rol no encontrado.' })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.service.remove(+id);
