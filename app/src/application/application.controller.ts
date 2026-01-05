b7fc8cbf62f492bdec6da5c43017df69e636b109
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-01-05 18:33:21 -0500

    feat: agregar documentación Swagger en ApplicationController y CreateApplicationDto

--- app/src/application/application.controller.ts
+++ app/src/application/application.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Post, Param, ParseIntPipe } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { CurrentUser, Roles } from 'src/common/decorator';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Applications')
 @Controller('vacancies')
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
 
+  @ApiOperation({ summary: 'Aplicar a una vacante' })
+  @ApiResponse({ status: 201, description: 'Aplicación exitosa.' })
+  @ApiResponse({ status: 404, description: 'Vacante no encontrada.' })
   @Post(':vacancyId/apply')
   @Roles('USER')
   async applyToVacancy(
--- app/src/application/dto/create-application.dto.ts
+++ app/src/application/dto/create-application.dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class CreateApplicationDto {
+  @ApiProperty({ description: 'ID del usuario que aplica', example: 1 })
   @IsNumber()
   userId: number;
 
+  @ApiProperty({
+    description: 'ID de la vacante a la que se aplica',
+    example: 10,
+  })
   @IsNumber()
   vacancyId: number;
 }
